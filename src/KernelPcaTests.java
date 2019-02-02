7f5bdd0ae214a08da802be2bb61bf2ff3dfaebc2
Jakub Cankudis <Jakub-Cc@users.noreply.github.com>
2019-02-02 10:38:33 +0100

    Test of Feature selection

--- src/KernelPcaTests.java
+++ src/KernelPcaTests.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 
+import core.SimpleMatrix;
 import jsat.SimpleDataSet;
 import jsat.classifiers.DataPoint;
 import jsat.datatransform.kernel.KernelPCA;
+import jsat.linear.DenseVector;
 
 public class KernelPcaTests {
 
@@ -10,11 +12,32 @@ public class KernelPcaTests {
 	System.out.println("test");
 
 	ArrayList<DataPoint> dataPoints = new ArrayList<DataPoint>();
+
+	SimpleMatrix matrix = new SimpleMatrix();
+	matrix.readDataFromCSV("te.csv");
+	for (int i = 0; i < matrix.pointsAmount; i++) {
+	    DenseVector numericalValues = new DenseVector(matrix.matrix[i]);
+	    dataPoints.add(new DataPoint(numericalValues));
+	}
+
 	SimpleDataSet dataSet = new SimpleDataSet(dataPoints);
 
 	KernelPCA kernelPCA = new KernelPCA(2);
-	kernelPCA.setBasisSize(1000);
+	kernelPCA.setBasisSize(10000);
 	kernelPCA.fit(dataSet);
+	System.out.println("fiting ended");
+
+	DataPoint datapoint;
+	SimpleMatrix matrix2 = new SimpleMatrix(dataPoints.size(), 2);
+	int i = 0;
+	for (DataPoint dp : dataPoints) {
+	    datapoint = kernelPCA.transform(dp);
+	    matrix2.matrix[i++] = datapoint.getNumericalValues().arrayCopy();
+	}
+
+	matrix2.saveToFile("KernelPCA.csv");
+
+	System.out.println("end");
     }
 
 }
---
+++ src/Test.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.IOException;
+
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.distance.PearsonCorrelationCoefficient;
+import net.sf.javaml.featureselection.ensemble.LinearRankingEnsemble;
+import net.sf.javaml.featureselection.ranking.RecursiveFeatureEliminationSVM;
+import net.sf.javaml.featureselection.scoring.GainRatio;
+import net.sf.javaml.featureselection.subset.GreedyForwardSelection;
+import net.sf.javaml.tools.data.FileHandler;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+	// TODO Auto-generated method stub
+
+	Dataset data = FileHandler.loadDataset(new File("te.csv"), ";");
+	System.out.println(data.size());
+	/* Construct a greedy forward subset selector */
+	GreedyForwardSelection ga = new GreedyForwardSelection(1, new PearsonCorrelationCoefficient());
+	/* Apply the algorithm to the data set */
+	ga.build(data);
+	/* Print out the attribute that has been selected */
+	System.out.println(ga.selectedAttributes());
+
+	System.out.println();
+	RecursiveFeatureEliminationSVM[] svmrfes = new RecursiveFeatureEliminationSVM[10];
+	for (int i = 0; i < svmrfes.length; i++)
+	    svmrfes[i] = new RecursiveFeatureEliminationSVM(0.2);
+	LinearRankingEnsemble ensemble = new LinearRankingEnsemble(svmrfes);
+	/* Build the ensemble */
+	ensemble.build(data);
+	/* Get rank of i-th feature */
+	for (int i = 0; i < 5; i++) {
+	    System.out.println(ensemble.rank(i));
+	}
+
+	System.out.println();
+	RecursiveFeatureEliminationSVM svmrfe = new RecursiveFeatureEliminationSVM(0.2);
+	/* Apply the algorithm to the data set */
+	svmrfe.build(data);
+	/* Print out the rank of each attribute */
+	for (int i = 0; i < svmrfe.noAttributes(); i++)
+	    System.out.println(svmrfe.rank(i));
+
+	System.out.println();
+	GainRatio gain = new GainRatio();
+	/* Apply the algorithm to the data set */
+	gain.build(data);
+	/* Print out the score of each attribute */
+	for (int i = 0; i < 5; i++)
+	    System.out.println(gain.score(i));
+    }
+
+}
