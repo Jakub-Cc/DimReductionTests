5ec95b7f7b83eeba76c36064f06ee550ade97d99
Jakub Cankudis <Jakub-Cc@users.noreply.github.com>
2019-02-22 20:17:52 +0100

    testing weka

--- src/AttributeSelectionTest.java
+++ src/AttributeSelectionTest.java
@@ -9,97 +9,103 @@ import weka.classifiers.meta.AttributeSelectedClassifier;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.core.Utils;
+import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
 import weka.filters.Filter;
 
 public class AttributeSelectionTest {
-	/**
-	 * uses the meta-classifier
-	 */
-	protected static void useClassifier(Instances data) throws Exception {
-		System.out.println("\n1. Meta-classfier");
-		AttributeSelectedClassifier classifier = new AttributeSelectedClassifier();
-		CfsSubsetEval eval = new CfsSubsetEval();
-		GreedyStepwise search = new GreedyStepwise();
-		search.setSearchBackwards(true);
-		J48 base = new J48();
-		classifier.setClassifier(base);
-		classifier.setEvaluator(eval);
-		classifier.setSearch(search);
-		Evaluation evaluation = new Evaluation(data);
-		evaluation.crossValidateModel(classifier, data, 10, new Random(1));
-		System.out.println(evaluation.toSummaryString());
-	}
+    /**
+     * uses the meta-classifier
+     */
+    protected static void useClassifier(Instances data) throws Exception {
+	System.out.println("\n1. Meta-classfier");
+	AttributeSelectedClassifier classifier = new AttributeSelectedClassifier();
+	CfsSubsetEval eval = new CfsSubsetEval();
+	GreedyStepwise search = new GreedyStepwise();
+	search.setSearchBackwards(true);
+	J48 base = new J48();
+	classifier.setClassifier(base);
+	classifier.setEvaluator(eval);
+	classifier.setSearch(search);
+	Evaluation evaluation = new Evaluation(data);
+	evaluation.crossValidateModel(classifier, data, 10, new Random(1));
+	System.out.println(evaluation.toSummaryString());
+    }
 
-	/**
-	 * uses the filter
-	 */
-	protected static void useFilter(Instances data) throws Exception {
-		System.out.println("\n2. Filter");
-		weka.filters.supervised.attribute.AttributeSelection filter = new weka.filters.supervised.attribute.AttributeSelection();
-		CfsSubsetEval eval = new CfsSubsetEval();
-		GreedyStepwise search = new GreedyStepwise();
-		search.setSearchBackwards(true);
-		filter.setEvaluator(eval);
-		filter.setSearch(search);
-		filter.setInputFormat(data);
-		Instances newData = Filter.useFilter(data, filter);
-		System.out.println(newData);
-	}
+    /**
+     * uses the filter
+     */
+    protected static void useFilter(Instances data) throws Exception {
+	System.out.println("\n2. Filter");
+	weka.filters.supervised.attribute.AttributeSelection filter = new weka.filters.supervised.attribute.AttributeSelection();
+	CfsSubsetEval eval = new CfsSubsetEval();
+	GreedyStepwise search = new GreedyStepwise();
+	search.setSearchBackwards(true);
+	filter.setEvaluator(eval);
+	filter.setSearch(search);
+	filter.setInputFormat(data);
+	Instances newData = Filter.useFilter(data, filter);
+	System.out.println(newData.toSummaryString());
 
-	/**
-	 * uses the low level approach
-	 */
-	protected static void useLowLevel(Instances data) throws Exception {
-		System.out.println("\n3. Low-level");
-		AttributeSelection attsel = new AttributeSelection();
-		CfsSubsetEval eval = new CfsSubsetEval();
-		GreedyStepwise search = new GreedyStepwise();
-		search.setSearchBackwards(true);
-		attsel.setEvaluator(eval);
-		attsel.setSearch(search);
-		attsel.SelectAttributes(data);
-		int[] indices = attsel.selectedAttributes();
-		System.out.println("selected attribute indices (starting with 0):\n" + Utils.arrayToString(indices));
-	}
+    }
 
-	/**
-	 * takes a dataset as first argument
-	 *
-	 * @param args
-	 *            the commandline arguments
-	 * @throws Exception
-	 *             if something goes wrong
-	 */
-	public static void main(String[] args) throws Exception {
-		// load data
-		System.out.println("\n0. Loading data");
-		// DataSource source = new DataSource("te3.csv");
+    /**
+     * uses the low level approach
+     */
+    protected static void useLowLevel(Instances data) throws Exception {
+	System.out.println("\n3. Low-level");
+	AttributeSelection attsel = new AttributeSelection();
+	CfsSubsetEval eval = new CfsSubsetEval();
+	GreedyStepwise search = new GreedyStepwise();
+	search.setSearchBackwards(true);
+	attsel.setEvaluator(eval);
+	attsel.setSearch(search);
+	attsel.SelectAttributes(data);
+	int[] indices = attsel.selectedAttributes();
+	System.out.println("selected attribute indices (starting with 0):\n" + Utils.arrayToString(indices));
+    }
 
-		// System.out.println(source.getClass());
-		// System.out.println(source.getLoader().getClass());
+    /**
+     * takes a dataset as first argument
+     *
+     * @param args the commandline arguments
+     * @throws Exception if something goes wrong
+     */
+    public static void main(String[] args) throws Exception {
+	// load data
+	System.out.println("\n0. Loading data");
+	// DataSource source = new DataSource("te3.csv");
 
-		CSVLoader loader = new CSVLoader();
-		loader.setSource(new File("te3.csv"));
+	// System.out.println(source.getClass());
+	// System.out.println(source.getLoader().getClass());
 
-		// Instances data = source.getDataSet();
-		Instances data = loader.getDataSet();
+	CSVLoader loader = new CSVLoader();
+	loader.setSource(new File("te3.csv"));
 
-		// if (data.classIndex() == -1)
-		// data.setClassIndex(data.numAttributes() - 1);
+	// Instances data = source.getDataSet();
+	Instances data = loader.getDataSet();
 
-		System.out.println(data.classIndex());
-		System.out.println(data.numAttributes());
+	// if (data.classIndex() == -1)
+	// data.setClassIndex(data.numAttributes() - 1);
 
-		data.setClassIndex(data.numAttributes() - 1);
+	System.out.println(data.classIndex());
+	System.out.println(data.numAttributes());
 
-		// 1. meta-classifier
-		// useClassifier(data);
+	data.setClassIndex(data.numAttributes() - 1);
 
-		// 2. filter
-		useFilter(data);
+	// 1. meta-classifier
+	// useClassifier(data);
 
-		// 3. low-level
-		useLowLevel(data);
-	}
+	// 2. filter
+	useFilter(data);
+
+	// 3. low-level
+	useLowLevel(data);
+
+	System.out.println();
+	ArffSaver saver = new ArffSaver();
+	saver.setInstances(data);
+	saver.setDestination(new File("art.arff"));
+	// saver.writeBatch();
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -11,45 +11,50 @@ import net.sf.javaml.tools.data.FileHandler;
 
 public class Test {
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) throws IOException {
+	// TODO Auto-generated method stub
 
-		Dataset data = FileHandler.loadDataset(new File("te.csv"), ";");
-		System.out.println(data.size());
-		/* Construct a greedy forward subset selector */
-		GreedyForwardSelection ga = new GreedyForwardSelection(2, new PearsonCorrelationCoefficient());
-		/* Apply the algorithm to the data set */
-		ga.build(data);
-		/* Print out the attribute that has been selected */
-		System.out.println(ga.selectedAttributes());
+	System.out.println("GreedyForwardSelection");
+	// Dataset data = FileHandler.loadDataset(new File("te4.csv"), ",");
+	Dataset data = FileHandler.loadDataset(new File("a.arff"), ",");
+	System.out.println(data.size());
+	System.out.println(data.get(0));
+	System.out.println(data.get(0).classValue());
 
-		System.out.println();
-		RecursiveFeatureEliminationSVM[] svmrfes = new RecursiveFeatureEliminationSVM[10];
-		for (int i = 0; i < svmrfes.length; i++)
-			svmrfes[i] = new RecursiveFeatureEliminationSVM(0.2);
-		LinearRankingEnsemble ensemble = new LinearRankingEnsemble(svmrfes);
-		/* Build the ensemble */
-		ensemble.build(data);
-		/* Get rank of i-th feature */
-		for (int i = 0; i < 5; i++) {
-			System.out.println(ensemble.rank(i));
-		}
+	/* Construct a greedy forward subset selector */
+	GreedyForwardSelection ga = new GreedyForwardSelection(1, new PearsonCorrelationCoefficient());
+	/* Apply the algorithm to the data set */
+	ga.build(data);
+	/* Print out the attribute that has been selected */
+	System.out.println(ga.selectedAttributes());
 
-		System.out.println();
-		RecursiveFeatureEliminationSVM svmrfe = new RecursiveFeatureEliminationSVM(0.2);
-		/* Apply the algorithm to the data set */
-		svmrfe.build(data);
-		/* Print out the rank of each attribute */
-		for (int i = 0; i < svmrfe.noAttributes(); i++)
-			System.out.println(svmrfe.rank(i));
-
-		System.out.println();
-		GainRatio gain = new GainRatio();
-		/* Apply the algorithm to the data set */
-		gain.build(data);
-		/* Print out the score of each attribute */
-		for (int i = 0; i < 5; i++)
-			System.out.println(gain.score(i));
+	System.out.println("\nRecursiveFeatureEliminationSVM [10]");
+	RecursiveFeatureEliminationSVM[] svmrfes = new RecursiveFeatureEliminationSVM[10];
+	for (int i = 0; i < svmrfes.length; i++)
+	    svmrfes[i] = new RecursiveFeatureEliminationSVM(0.2);
+	LinearRankingEnsemble ensemble = new LinearRankingEnsemble(svmrfes);
+	/* Build the ensemble */
+	ensemble.build(data);
+	/* Get rank of i-th feature */
+	for (int i = 0; i < 5; i++) {
+	    System.out.println(ensemble.rank(i));
 	}
+
+	System.out.println("\nRecursiveFeatureEliminationSVM 0.2");
+	RecursiveFeatureEliminationSVM svmrfe = new RecursiveFeatureEliminationSVM(0.5);
+	/* Apply the algorithm to the data set */
+	svmrfe.build(data);
+	/* Print out the rank of each attribute */
+	for (int i = 0; i < svmrfe.noAttributes(); i++)
+	    System.out.println(svmrfe.rank(i));
+
+	System.out.println("\nGainRatio");
+	GainRatio gain = new GainRatio();
+	/* Apply the algorithm to the data set */
+	gain.build(data);
+	/* Print out the score of each attribute */
+	for (int i = 0; i < 5; i++)
+	    System.out.println(gain.score(i));
+    }
 
 }
