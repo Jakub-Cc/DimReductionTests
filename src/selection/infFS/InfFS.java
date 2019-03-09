eb02434bcf9a57f980fbbec08a29f5d8ae85a9d8
Jakub Cankudis <Jakub-Cc@users.noreply.github.com>
2019-03-09 21:07:36 +0100

    implemented InfFS - need test is used methods in it work

--- src/selection/infFS/InfFS.java
+++ src/selection/infFS/InfFS.java
@@ -5,43 +5,78 @@ import selection.infFS.methods.MatrixTools;
 
 public class InfFS {
 
-	public int[] infFS(SimpleMatrix Xtrain, double alpha, boolean verbose) {
+    public static void main(String[] args) {
+	SimpleMatrix matrix = new SimpleMatrix();
 
-		int[] ranked = new int[Xtrain.dimensions];
+	matrix.readDataFromCSV("x.csv");
+	System.out.println();
 
-		SimpleMatrix corr = MatrixTools.spearmanCorrelation(Xtrain);
-		MatrixTools.replaceUndef(corr.matrix, 0);
-		MatrixTools.transformValues(corr.matrix, x -> 1 - Math.abs(x));
+	long start = System.currentTimeMillis();
+	{
+	    InfFS infFS = new InfFS();
+	    int[] rank = infFS.infFS(matrix, 0.5, false);
 
-		double[] STD = MatrixTools.standardDeviation(Xtrain.matrix, false);
-		double[][] sigma = MatrixTools.bsxfun((a, b) -> a > b ? a : b, STD, STD);
-		double min = MatrixTools.min(sigma);
-		MatrixTools.transformValues(sigma, e -> e - min);
-
-		double max = MatrixTools.max(sigma);
-		MatrixTools.transformValues(sigma, e -> e / max);
-		MatrixTools.replaceUndef(sigma, 0);
-
-		MatrixTools.transformValues(corr.matrix, x -> alpha * x);
-		MatrixTools.transformValues(sigma, x -> (1 - alpha) * x);
-
-		double[][] A = MatrixTools.useFunctionOnMatrixes((a, b) -> a + b, corr.matrix, sigma);
-
-		double[][] I = MatrixTools.eye(A.length);
-
-		double factor = 0.99;
-		double r = (factor / MatrixTools.max(MatrixTools.eigenValues(A)));
-
-		MatrixTools.transformValues(A, x -> x * r);
-		double[][] y = MatrixTools.useFunctionOnMatrixes((a, b) -> a - b, I, A);
-
-		double[][] S = MatrixTools.useFunctionOnMatrixes((a, b) -> a - b, MatrixTools.inverse(y), I);
-
-		double[] weight = MatrixTools.sumOfElements(S, false);
-
-		ranked = MatrixTools.sortDescendant(weight);
-
-		return ranked;
+	    for (int i = 0; i < rank.length; i++) {
+		System.out.println(rank[i]);
+	    }
 	}
+	long elapsedTime = System.currentTimeMillis() - start;
+	System.out.println();
+	System.out.println((double) elapsedTime / 1000);
+    }
+
+    public int[] infFS(SimpleMatrix Xtrain, double alpha, boolean verbose) {
+
+	int[] ranked = new int[Xtrain.dimensions];
+
+	SimpleMatrix corr = MatrixTools.spearmanCorrelation(Xtrain);
+	MatrixTools.replaceUndef(corr.matrix, 0);
+	MatrixTools.transformValues(corr.matrix, x -> 1 - Math.abs(x));
+
+	System.out.println("corelation done");
+
+	double[] STD = MatrixTools.standardDeviation(Xtrain.matrix, false);
+	double[][] sigma = MatrixTools.bsxfun((a, b) -> a > b ? a : b, STD, STD);
+
+	System.out.println("STD done");
+
+	double min = MatrixTools.min(sigma);
+	MatrixTools.transformValues(sigma, e -> e - min);
+
+	double max = MatrixTools.max(sigma);
+	MatrixTools.transformValues(sigma, e -> e / max);
+	MatrixTools.replaceUndef(sigma, 0);
+
+	MatrixTools.transformValues(corr.matrix, x -> alpha * x);
+	MatrixTools.transformValues(sigma, x -> (1 - alpha) * x);
+
+	double[][] A = MatrixTools.useFunctionOnMatrixes((a, b) -> a + b, corr.matrix, sigma);
+
+	double[][] I = MatrixTools.eye(A.length);
+	System.out.println("A done");
+
+	// MatrixTools.printMatrix(A);
+
+	double factor = 0.99;
+	double r = (factor / MatrixTools.max(MatrixTools.eigenValues(A)));
+	System.out.println("Eigen done");
+
+	MatrixTools.transformValues(A, x -> x * r);
+	double[][] y = MatrixTools.useFunctionOnMatrixes((a, b) -> a - b, I, A);
+
+	double[][] S = MatrixTools.useFunctionOnMatrixes((a, b) -> a - b, MatrixTools.inverse(y), I);
+
+	double[] weight = MatrixTools.sumOfElements(S, false);
+	System.out.println("weights done");
+
+	for (int i = 0; i < weight.length; i++) {
+	    System.out.print("" + i + " " + weight[i] + " ;");
+	}
+	System.out.println();
+
+	ranked = MatrixTools.sortDescendant(weight);
+	System.out.println("sort done");
+	return ranked;
+    }
 
 }
