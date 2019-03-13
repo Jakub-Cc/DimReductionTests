8d321112c9fb2818dcc4bd2efeeb330a0a9d7e98
Jakub Cankudis <Jakub-Cc@users.noreply.github.com>
2019-03-13 09:49:30 +0100

    Implemented laplacian score FS

--- src/myImp/LaplacianScore.java
+++ src/myImp/LaplacianScore.java
@@ -6,74 +6,76 @@ import distanceMeasures.Euclidean;
 
 public class LaplacianScore {
 
-	public static void main(String[] args) {
-		SimpleMatrix matrix = new SimpleMatrix();
+    public static void main(String[] args) {
+	SimpleMatrix matrix = new SimpleMatrix();
 
-		matrix.readDataFromCSV("x.csv");
-		System.out.println();
+	matrix.readDataFromCSV("x.csv");
+	System.out.println();
 
-		long start = System.currentTimeMillis();
-		{
+	long start = System.currentTimeMillis();
+	{
 
-			int[] rank = laplacianScore(matrix.matrix);
+	    int[] rank = laplacianScore(matrix.matrix);
 
-			for (int i = 0; i < rank.length; i++) {
-				System.out.println(rank[i]);
-			}
-		}
-		long elapsedTime = System.currentTimeMillis() - start;
-		System.out.println();
-		System.out.println((double) elapsedTime / 1000);
-
+	    for (int i = 0; i < rank.length; i++) {
+		System.out.println(rank[i]);
+	    }
 	}
+	long elapsedTime = System.currentTimeMillis() - start;
+	System.out.println();
+	System.out.println((double) elapsedTime / 1000);
 
-	public static int[] laplacianScore(double[][] matrix) {
+    }
 
-		double[][] distanceMatrix = MatrixTools.generateDistanceMatrix(matrix, new Euclidean());
-		double max = MatrixTools.max(distanceMatrix);
-		MatrixTools.transformValues(distanceMatrix, x -> -(x / max));
+    public static int[] laplacianScore(double[][] matrix) {
 
-		double[] sum = MatrixTools.sumOfElements(distanceMatrix, false);
+	double[][] distanceMatrix = MatrixTools.generateDistanceMatrix(matrix, new Euclidean());
+	double max = MatrixTools.max(distanceMatrix);
+	MatrixTools.transformValues(distanceMatrix, x -> -(x / max));
 
-		double[][] sumMatrix = MatrixTools.vectorToMatrix(sum);
-		double[][] sumMatrixT = MatrixTools.transpose(sumMatrix);
+	double[] sum = MatrixTools.sumOfElements(distanceMatrix, false);
+	double[][] sumMatrix = MatrixTools.vectorToMatrix(sum, false);
 
-		double[][] tmpl = MatrixTools.multiplicateMatrixes(sumMatrixT, matrix);
+	double[][] tmpl = MatrixTools.multiplicateMatrixesWrap(sumMatrix, matrix);
 
-		double[][] D = MatrixTools.transformToDiagonalMatrix(sum);
+	double sumOfAllElements = MatrixTools.sumOfElements(sum);
+	MatrixTools.transformValues(tmpl, x -> x * x / sumOfAllElements);
 
-		double sumOfAllElements = MatrixTools.sumOfElements(sum);
-		MatrixTools.transformValues(tmpl, x -> x * x / sumOfAllElements);
+	double[][] DPrime = calculateDPrime(matrix, sum);
+	DPrime = MatrixTools.useFunctionOnMatrixes((a, b) -> a - b, DPrime, tmpl);
 
-		double[][] DPrime = calculatePrime(matrix, D);
-		DPrime = MatrixTools.useFunctionOnMatrixes((a, b) -> a - b, DPrime, tmpl);
+	double[][] LPrime = calculateLPrime(matrix, distanceMatrix);
+	LPrime = MatrixTools.useFunctionOnMatrixes((a, b) -> a - b, LPrime, tmpl);
 
-		double[][] LPrime = calculatePrime(matrix, distanceMatrix);
-		LPrime = MatrixTools.useFunctionOnMatrixes((a, b) -> a - b, LPrime, tmpl);
+	MatrixTools.transformValues(DPrime, x -> (x < 1e-12) ? 10000 : x);
 
-		MatrixTools.transformValues(DPrime, x -> (x < 1e-12) ? 10000 : x);
+	double[][] laplacianScores = MatrixTools.useFunctionOnMatrixes((a, b) -> a / b, LPrime, DPrime);
+	laplacianScores = MatrixTools.transpose(laplacianScores);
 
-		double[][] laplacianScores = MatrixTools.useFunctionOnMatrixes((a, b) -> a / b, LPrime, DPrime);
-		laplacianScores = MatrixTools.transpose(laplacianScores);
+	int[] ranking = MatrixTools.sortDescendant(MatrixTools.dropSingleton(laplacianScores));
+	return ranking;
 
-		int[] ranking = MatrixTools.sortDescendant(MatrixTools.dropSingleton(laplacianScores));
+    }
 
-		return ranking;
-	}
+    private static double[][] calculateDPrime(double[][] X, double[] D) {
+	double[][] DPrime = MatrixTools.useFunctionRowWise(X, D, (a, b) -> a * b);
 
-	private static double[][] calculatePrime(double[][] elementsMatrix, double[][] multiplicantMatrix) {
-		double[][] elementsMatrixTrans = MatrixTools.transpose(elementsMatrix); // X'
+	DPrime = MatrixTools.useFunctionOnMatrixesTransposeA((a, b) -> a * b, DPrime, X);
 
-		double[][] LPrime = MatrixTools.multiplicateMatrixes(elementsMatrixTrans, multiplicantMatrix); // X'*L
+	double[] sumOfLPrime = MatrixTools.sumOfElements(DPrime, false);
 
-		LPrime = MatrixTools.transpose(LPrime); // (X'*L)'
-		LPrime = MatrixTools.useFunctionOnMatrixes((a, b) -> a * b, LPrime, elementsMatrix); // (X'*L)'.*X
+	return MatrixTools.vectorToMatrix(sumOfLPrime, false);
+    }
 
-		double[] sumOfLPrime = MatrixTools.sumOfElements(LPrime, false); // sum((X'*D)'.*X)
+    private static double[][] calculateLPrime(double[][] elementsMatrix, double[][] multiplicantMatrix) {
+	double[][] elementsMatrixTrans = MatrixTools.transpose(elementsMatrix); // X'
 
-		double[][] matrixOfSumOfLPrime = MatrixTools.vectorToMatrix(sumOfLPrime);
-		matrixOfSumOfLPrime = MatrixTools.transpose(matrixOfSumOfLPrime);
+	double[][] LPrime = MatrixTools.multiplicateMatrixesWrap(elementsMatrixTrans, multiplicantMatrix);
 
-		return matrixOfSumOfLPrime;
-	}
+	LPrime = MatrixTools.useFunctionOnMatrixesTransposeA((a, b) -> a * b, LPrime, elementsMatrix); // (X'*L)'.*X
+
+	double[] sumOfLPrime = MatrixTools.sumOfElements(LPrime, false); // sum((X'*D)'.*X)
+
+	return MatrixTools.vectorToMatrix(sumOfLPrime, false);
+    }
 }
